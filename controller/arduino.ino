c6a6b82b57be69ec222fea7170e0c147db090aae
Денис Заречнев <d.zarech@gmail.com>
2022-05-07 14:18:56 +0300

    Соединил скрипт контроллера

--- controller/arduino.ino
+++ controller/arduino.ino
@@ -1,3 +1,11 @@
+double kp=1,
+       kl=0,
+       kd=0;
+unsigned long currentTime, previousTime;
+double elapsedTime;
+double error;
+double input, output, setPoint;
+double cumError, rstePoint;
 
 
 #include <Servo.h>
@@ -15,6 +23,12 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT); //задаем режим работы на вывод данных для контакта 13, к которому подключен светодиод
   digitalWrite (LED_BUILTIN, LOW);
   Serial.println("Hi!, I am Arduino");
+
+  setPoint = analogRead(A6);
+    input = analogRead(A7);
+    output = computePID(input);
+    delay(100);
+    analogWrite(3, output);
   
   servoX.attach(9);
   servoY.attach(8);
@@ -25,25 +39,47 @@ int[] readCoordinates() {
     int v = 0;
     while (Serial.available() > 0) {
         char data = Serial.read();
-//       if(data == '1') {
-//         v = v*2 + 1;
-//       }
-//       if(data == '0') {
-//         v = v*2;
-//       }
-        v = 2*v + atoi(data);
+      if(data == '1') {
+        v = v*2 + 1;
+      }
+      if(data == '0') {
+        v = v*2;
+      }
     }
     coordinates[0] = v/1024;
     coordinates[1] = v%1024;
 }
+
+double computePID(double inp, int target){
+  currentTime = millis();
+  elapsedTime = (double)(currentTime - previousTime);
+
+  error = target - inp;
+  cumError += error * elapsedTime;
+  rateError = (error - lastError)/elapsedTime;
+
+  double out = kp * error + kl * cumError + kd * rateError;
+
+  lastError = error;
+  previousTime = currentTime;
+
+  return out;
+}
  
 void loop() {
-  int[2] coordinates = readCoordinates()
-  int x = coordinates[0];
-  int y = coordinates[1];
 
-  servoX.write();
-  servoY.write();
+    int targetX = 0;
+    int targetY = 0;
 
-  delay(1000);
+    outputX = computePID(x, targetX);
+    outputY = computePID(y, targetY);
+
+    int[2] coordinates = readCoordinates()
+    int x = coordinates[0];
+    int y = coordinates[1];
+
+    servoX.write(outputX);
+    servoY.write(outputY);
+
+    delay(100);
 }
--- controller/error.ino
+++ controller/error.ino
@@ -1,38 +1,38 @@
-double kp=1,
-       kl=0, 
-       kd=0;
-unsigned long currentTime, previousTime;
-double elapsedTime;
-double erroe;
-double input, output, setPoint;
-double cumError, rstePoint;
-
-void setup() {
-  
-
-}
-
-void loop() {
-  setPoint = analogRead(A6);
-    input = analogRead(A7);
-    output = computePID(input);
-    delay(100);
-    analogWrite(3, output);
-    
-}
-
-double computePID(double inp){
-  currentTime = millis();
-  elapsedTime = (double)(currentTime - previousTime);
-
-  error = Setpoint - inp;
-  cumError += error * elapsedTime;
-  rateError = (error - lastError)/elapsedTime;
-
-  double out = kp * error + kl * cumError + kd * rateError;
-
-  lastError = error;
-  previousTime = currentTime;
-
-  return out;
-}
+// double kp=1,
+//        kl=0,
+//        kd=0;
+// unsigned long currentTime, previousTime;
+// double elapsedTime;
+// double error;
+// double input, output, setPoint;
+// double cumError, rstePoint;
+//
+// void setup() {
+//
+//
+// }
+//
+// void loop() {
+//   setPoint = analogRead(A6);
+//     input = analogRead(A7);
+//     output = computePID(input);
+//     delay(100);
+//     analogWrite(3, output);
+//
+// }
+//
+// double computePID(double inp){
+//   currentTime = millis();
+//   elapsedTime = (double)(currentTime - previousTime);
+//
+//   error = Setpoint - inp;
+//   cumError += error * elapsedTime;
+//   rateError = (error - lastError)/elapsedTime;
+//
+//   double out = kp * error + kl * cumError + kd * rateError;
+//
+//   lastError = error;
+//   previousTime = currentTime;
+//
+//   return out;
+// }
