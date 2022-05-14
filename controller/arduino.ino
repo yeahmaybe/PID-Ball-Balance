20304d6822da9699c6c5f62f89cc20b457740167
Денис Заречнев <d.zarech@gmail.com>
2022-05-14 22:10:33 +0300

    шевелим лапками но мяч пляшет

--- controller/arduino.ino
+++ controller/arduino.ino
@@ -1,13 +1,16 @@
 double kp=1,
-kl=0.01,
-kd=0.1;
+kl=0,
+kd=0;
 unsigned long currentTime, previousTime;
 double elapsedTime;
 double error;
 double input, outputX, outputY, setPoint;
 double cumError, rateError, lastError;
 long coordinates[2];
-String data ;
+int minAngle = 60,
+    maxAngle = 120,
+    dispersion = 300;
+String data;
 
 double x, y, targetX, targetY;
 
@@ -18,7 +21,7 @@ Servo servoY; // create servo object to control a
 
 void setup() {
 Serial.begin(115200); //инициализируем последовательную связь на COM порту со скоростью 9600 бод/с
-Serial.print("Hi!, I am Arduino");
+//Serial.print("Hi!, I am Arduino");
 
 servoX.attach(9);
 servoY.attach(8);
@@ -46,42 +49,44 @@ void readCoordinates() {
 }
 
 double computePID(double inp, double target){
-//  currentTime = millis();
-//  elapsedTime = (double)(currentTime - previousTime);
-//
-    error = target - inp;
-//  cumError += error * elapsedTime;
-//  rateError = (error - lastError)/elapsedTime;
+  currentTime = millis();
+  elapsedTime = (double)(currentTime - previousTime);
+
+  error = target - inp;
+  cumError += error * elapsedTime;
+  rateError = (error - lastError)/elapsedTime;
 
   double out = kp * error + kl * cumError + kd * rateError;
 
   lastError = error;
   previousTime = currentTime;
-  return error;
+  return out;
 
 }
 
 void loop() {
-
-
-targetX = 650;
-targetY = 350;
+targetX = 1000;
+targetY = 500;
 readCoordinates();
 
 x = coordinates[0];
 y = coordinates[1];
 outputX = computePID(x, targetX);
 outputY = computePID(y, targetY);
-int outX = map(outputX, -300, 300, 10, 170);
-int outY = map(outputY, -300, 300, 10, 170);
+int outX = constrain(map(outputX, -dispersion, dispersion, minAngle, maxAngle), minAngle, maxAngle);
+int outY = constrain(map(outputY, -dispersion, dispersion, minAngle, maxAngle), minAngle, maxAngle);
 
-Serial.print(outX);
-Serial.print(" ");
-Serial.println(outY);
+//Serial.println(x);
+//Serial.print(" ");
+//Serial.println(outputY);
+//
+//Serial.println(outX);
+//Serial.print(" ");
+//Serial.println(outY);
 
 
-//servoX.write(90);
-//servoY.write(90);
+servoX.write(outX);
+servoY.write(180-outY);
 
-delay(300);
+delay(200);
 }
